ca6ed5fe23d5b14178c4dd6bb37b8fde059d98f8
Meghana Putta <meghanareddy.putta@gmail.com>
2019-02-07 20:40:26 -0600

    update

---
+++ src/Web/Controllers/DiaryController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace dotnet_mvc_sample.Controllers
+{
+    public class DiaryController : Controller
+    {
+        public IActionResult Textmethod()
+        {
+            return Content("This is Textmethod");
+        }
+
+        public IActionResult Jsonmethod()
+        {
+            return Json(new { name = "JSON", description = "This is controller2" });
+        }
+
+        public IActionResult Htmlmethod()
+        {
+            return Content("<html> <body> <h1> This is Htmlmethod </h1> </body> </html>","text/html");
+        }
+
+        public IActionResult Viewmethod()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
