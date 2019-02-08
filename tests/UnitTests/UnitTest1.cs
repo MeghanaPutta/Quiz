410a66615845ad01860b2252e3e4c344081ca17d
Meghana Putta <meghanareddy.putta@gmail.com>
2019-02-07 20:25:48 -0600

    commit

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,32 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void square_WorksWithproductofanumber()
         {
-
+        var expected = 4;
+        var num = 2;
+        var actual = square(num);
+        Assert.Equal(expected,actual);
         }
+
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-3)]
+        [InlineData(-4)]
+        [InlineData(-5)]
+        public void Isnegative_WorksWithanumber(int n)
+        {
+        Assert.True(Isnegative(n));
+        }
+        int square(int num)
+            {
+            return num*num;
+        }           
+        bool Isnegative(int n)
+        {
+            return n < 0;
+        }
+
     }
 }
